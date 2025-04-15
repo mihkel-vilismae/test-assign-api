6b322c070166212f85d2b21aea418722d76219da
Mihkel Vilismäe <mihkel.vilismae@gmail.com>
2025-04-15 17:03:14 +0300

    Fix AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Criteria;
+use App\Entity\Criterion;
 use App\Entity\Filter;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,17 +17,17 @@ class AppFixtures extends Fixture
         $filter1->setName('Product Filter');
         $filter1->setSelection('common');
 
-        $criteria1a = new Criteria();
+        $criteria1a = new Criterion();
         $criteria1a->setType('amount');
         $criteria1a->setComparator('>');
         $criteria1a->setValue('50');
-        $filter1->addCriteria($criteria1a);
+        $filter1->addCriterion($criteria1a);
 
-        $criteria1b = new Criteria();
+        $criteria1b = new Criterion();
         $criteria1b->setType('title');
         $criteria1b->setComparator('starts_with');
         $criteria1b->setValue('Samsung');
-        $filter1->addCriteria($criteria1b);
+        $filter1->addCriterion($criteria1b);
 
         $manager->persist($filter1);
 
@@ -36,17 +37,17 @@ class AppFixtures extends Fixture
         $filter2->setName('Customer Filter');
         $filter2->setSelection('special');
 
-        $criteria2a = new Criteria();
+        $criteria2a = new Criterion();
         $criteria2a->setType('date');
         $criteria2a->setComparator('to');
         $criteria2a->setValue('2022-01-01');
-        $filter2->addCriteria($criteria2a);
+        $filter2->addCriterion($criteria2a);
 
-        $criteria2b = new Criteria();
+        $criteria2b = new Criterion();
         $criteria2b->setType('amount');
         $criteria2b->setComparator('>');
         $criteria2b->setValue('1000');
-        $filter2->addCriteria($criteria2b);
+        $filter2->addCriterion($criteria2b);
 
         $manager->persist($filter2);
 
